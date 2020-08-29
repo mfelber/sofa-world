36d2a83325b1d3f9f7d2f72c89fc2568c8e0f68e
janfelber <68186567+janfelber@users.noreply.github.com>
2020-08-29 17:45:58 +0200

    styl

--- header.php
+++ header.php
@@ -1,16 +1,17 @@
 <?php
-        echo '<header>';
-        echo '<h1 class="logo"></h1>';
-        echo '<input type="checkbox" id ="nav-toggle"class= "nav-toggle">';
-        echo '<nav>';
-        echo '<ul>';
-        echo '<li><a href="#">Home</a></li>';
-        echo '<li><a href="#">Menu</a></li>';
-        echo '<li><a href="#">Location</a></li>';
-        echo '<li><a href="#">Contact</a></li>';
-        echo '</ul>';
-        echo '</nav>';
-        echo '<label for="nav-toggle" class = "nav-toggle-label">';
-        echo '</label>';
-        echo '</header>';
+    echo '<nav>';
+    echo '<div class="logo">';
+    echo 'Brand</div>';
+    echo '<input type="checkbox" id="click">';
+    echo '<label for="click" class="menu-btn">';
+    echo' <i class="fas fa-bars"></i>';
+    echo '</label>';
+    echo '<ul>';
+    echo '<li><a class="active" href="index.php">Home</a></li>';
+    echo '<li><a href="menu1.php">Menu</a></li>';
+    echo '<li><a href="#">Services</a></li>';
+    echo '<li><a href="location.php">Location</a></li>';
+    echo '<li><a href="contact.php">Contact</a></li>';
+    echo '</ul>';
+    echo '</nav>';
 ?> 
